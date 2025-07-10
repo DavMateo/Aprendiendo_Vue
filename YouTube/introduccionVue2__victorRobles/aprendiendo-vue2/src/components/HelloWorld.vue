9bc4b540f777e030a0d1ee336e0313f50a8b1eaf
DavMateo <davidcarrenomc@gmail.com>
2025-07-10 14:55:21 -0500

    feat: :art: Reestructuración carpetas de cursos

---
+++ YouTube/introduccionVue2__victorRobles/aprendiendo-vue2/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>{{ texto }}</h2>
+
+  <div style="background: red">
+    <MiComponente />
+  </div>
+</template>
+
+<script>
+import MiComponente from './MiComponente.vue';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    MiComponente
+  },
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      texto: "Hola Mundo desde el componente!!"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ YouTube/introduccionVue2__victorRobles/aprendiendo-vue2/src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="mi-componente">
+        <hr />
+        <h1>Este es <strong>MI COMPONENTE</strong> </h1>
+        <h2>{{ subtitulo }}</h2>
+        <hr />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MiComponente",
+        data() {
+            return {
+                subtitulo: "Soy el subtítulo de mi componente"
+            }
+        }
+    }
+</script>
